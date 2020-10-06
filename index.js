dbe63017c94d11590e9ac90e04d0ace9bffa09b2
minhoo03 <minhoo0333@gmail.com>
2020-10-07 01:01:02 +0900

    Connected MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+const e = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://gcy:`kimbum1976`@boiler-plate.txkec.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => console.log("Hello World!"));
 
 app.listen(port, () => console.log(`Server Open! -> http://localhost:${port}`));
+
+// PS C:\Users\김민후\boiler-plate> git remote add origin https://github.com/minhoo03/boilerplate.git
+// PS C:\Users\김민후\boiler-plate> git push origin master --set-upstream
